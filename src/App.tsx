32df8dc7210f5611008c2a31d261a8f483667170
Walter Fritsche <wfritsche95@protonmail.com>
2024-07-23 11:55:14 -0500

    git init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <main>
+      <div>
+        <h1 className="text-3xl">Tailwind fontSize Extender</h1>
+      </div>
+    </main>
+  );
+}
+
+export default App;
